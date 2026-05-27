72e4ba86d6bcce7e28484908807976f3458757cc
Ali10-star <alosharoos@gmail.com>
2026-05-28 00:17:15 +0400

    refactor: replace custom class-merging logic with clsx and tailwind-merge dependency

--- lib/cn.ts
+++ lib/cn.ts
@@ -1,8 +1,6 @@
-// lib/cn.ts
-// Tiny class-merging helper used throughout the Bauhaus components.
-// If you're using shadcn/ui you'll likely already have this; this version
-// avoids the clsx/tailwind-merge dependency for simplicity.
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...classes: Array<string | false | null | undefined>): string {
-  return classes.filter(Boolean).join(" ");
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
